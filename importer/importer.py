262fc30252c913115dfdc7392246d83b8f1e4046
Mark Baggett <mbagget1@utk.edu>
2022-12-27 15:50:51 -0500

    Get pagination.

--- importer/importer.py
+++ importer/importer.py
@@ -2,7 +2,8 @@ from requestium import Session, Keys
 from selenium import webdriver
 import yaml
 
-class ImporterReviewer():
+
+class ImporterReviewer:
     def __init__(self, initial_auth=None, hyku_instance='https://dc.utk-hyku-production.notch8.cloud'):
         self.s = Session(
             webdriver_path='/usr/local/bin/chromedriver',
@@ -24,12 +25,14 @@ class ImporterReviewer():
         return self.s.driver.save_screenshot(f'{output}.png')
 
     def access_importer(self, importer):
-        return self.s.driver.get(f'https://dc.utk-hyku-production.notch8.cloud/importers/{importer}?locale=en')
-
+        errors = []
+        self.s.driver.get(f'https://dc.utk-hyku-production.notch8.cloud/importers/{importer}?locale=en')
+        results = [link.text for link in self.s.driver.find_elements_by_xpath('//tr')]
+        pagination = [link.get_attribute('href') for link in self.s.driver.find_elements_by_xpath('//ul[@class="pagination"]/li/a')]
+        return pagination
 
 if __name__ == "__main__":
     settings = yaml.safe_load(open('settings.yml'))
     x = ImporterReviewer((settings['user'], settings['password']))
     x.sign_in_to_hyku(settings['hyku_user'], settings['hyku_password'])
-    x.access_importer(25)
-    x.take_screenshot('mark')
+    print(x.access_importer(25))
